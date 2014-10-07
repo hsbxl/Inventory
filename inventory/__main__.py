c27d9b473eb20f17a0d3d8f40c77001d1b6a8910
OKso <git@okso.me>
2014-10-07 23:47:15 +0200

    [add] Created base Server-side backend

---
+++ inventory/__main__.py
@@ -0,0 +1,30 @@
+"""
+HSBXL inventory web app.
+"""
+
+import os.path
+import json
+from bottle import Bottle, template, static_file, response
+
+bottle = Bottle()
+
+HERE = os.path.dirname(os.path.realpath(__file__))
+
+
+@bottle.route('/')
+def index():
+    return template(os.path.join(HERE, 'views/index'))
+
+
+@bottle.route('/object/<name>')
+def index(name):
+    response.content_type = 'application/json'
+    return json.dumps({"hello": "stuff"})
+
+
+@bottle.route('/static/<filename>')
+def server_static(filename):
+    return static_file(filename, root=os.path.join(HERE, 'static'))
+
+
+bottle.run(debug=True, reloader=True, host='localhost', port=8080)
---
+++ inventory/views/index.tpl
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+    <h1>HSBXL Inventory</h1>
+    <p>
+        This is the hackerspace's inventory.
+        <a href="https://github.com/hsbxl/Inventory">Hack on it !</a>
+    </p>
+    <p>
+        Have a look at <a href='/object/some_stuff'>some stuff</a>.
+    </p>
+</body>
+</html>
